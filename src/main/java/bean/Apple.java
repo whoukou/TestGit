2ce2cfc133c2c6f5037a7a50ee6eaee031ea9e47
wh_glb <wh_glb@qq.com>
2019-08-24 13:24:45 +0800

    hot fix

--- src/main/java/bean/Apple.java
+++ src/main/java/bean/Apple.java
@@ -4,8 +4,8 @@ public class Apple {
 	public static void main(String[] args) {
 
 		System.out.println("apple new");
-
-		System.out.println("apple old");
+		
+		System.out.println("hot fix");
 
 	}
 }
